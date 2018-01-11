7c2786708bee1b34e740b548c345a1c3578e47a1
Augusto Lucas <augustolucas92@gmail.com>
2018-01-11 00:13:59 -0200

    primeiras alterações front

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/*import registerServiceWorker from './registerServiceWorker';*/
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/*var button = [
+	{id: 1, label: "Cadastro de Produto"},
+	{id: 2, label: "Registrar Pedido"},
+	{id: 3, label: "Novo"},
+	{id: 4, label: "Salvar"}
+];*/
+
+/*Função criar botão*/
+var CallButton = (props) => (
+	<button id={props.id} class={props.class}>{props.label}</button>
+);
+
+class MainApp extends React.Component{
+	render(){
+		return(
+			<div>
+				<h1>Subwine</h1>
+				<div>
+					<CallButton id="1" class="btn-01" label="Cadastro de Produto" />
+					<CallButton id="2" class="btn-02" label="Registrar Pedido" />
+				</div>
+
+
+
+				
+
+			</div>
+		);
+	}
+};
+
+
+ReactDOM.render(
+	<MainApp />,
+	document.getElementById('root')
+);
+/*registerServiceWorker();*/
